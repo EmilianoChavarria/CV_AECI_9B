dc1a3f6db07699a58ba20c3ed853a6a10421107a
EmilianoChavarria <20223tn089@utez.edu.mx>
2025-05-27 16:06:32 -0600

    Initial commit

---
+++ src/main/java/utez/edu/mx/cvaeci9b/Cvaeci9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvaeci9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvaeci9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvaeci9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvaeci9b/Cvaeci9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvaeci9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvaeci9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
